77bdd84770e13076b3f08b0605698ac59b0482d8
unknown <katemeng08@gmail.com>
2022-02-28 17:38:19 -0800

    added route

--- src/App.js
+++ src/App.js
@@ -85,6 +85,7 @@ class App extends Component {
             <Nav />
             <div className="main">
               <Routes>
+                <Route exact path="/" element={<Home />} />
                 <Route exact path="/portfolio/" element={<Home />} />
                 <Route path="/portfolio/about" element={<About />} />
                 <Route path="/portfolio/projects" element={<Projects />} />
